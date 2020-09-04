c6d017d4eab85e7f177504f59d0dde2020fee1b7
csocopc <jsocopc@gmail.com>
2020-09-04 15:24:23 -0600

    Pagina principal actualizada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">{{ __('DIACO') }}</div>
+                    <div class="card-body">
+                        <h1 class="cover-heading">DIACO Quejas Online.</h1>
+                        <p class="lead">Bienvenido a la pagina de administracion de quejas online de la DIACO</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
