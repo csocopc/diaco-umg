57d1ce3a845971ae9c3b2dede9e4cc68597d1aa6
csocopc <jsocopc@gmail.com>
2020-09-30 00:02:13 -0600

    Archivo de rutas web actualizado

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
 
 // Rutas de la entidad 'Comercio'
 Route::get('/comercios/index/{q?}', 'ComerciosController@index')->middleware('auth');
@@ -30,11 +17,9 @@ Route::get('/reportes', 'ReportesController@index')->middleware('auth');
 Route::post('/reportes/obtener', 'ReportesController@obtenerDatos')->middleware('auth');
 Route::get('/reportes/obtener', 'ReportesController@obtenerDatos')->middleware('auth');
 
-
 // Rutas para quejas
 Route::get('/quejas/index/{q?}', 'QuejasController@index')->middleware('auth');
 Route::get('/quejas/detalles/{id}', 'QuejasController@detalles')->middleware('auth');
-
 
 /////// Paginas publicas
 // Consumidor
